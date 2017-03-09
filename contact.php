7d08f9d29b157e0aa4f2f09d1e307e14d558bd72
Joshua Huffman <j.m.huffman@gmail.com>
2017-03-09 13:01:42 -0800

    add contact page

---
+++ contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Contact Me</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+    <link rel='stylesheet' href='css/styles.css' type='text/css'>
+  </head>
+  <body>
+      <div class="text-center">
+          <br>
+          <h1>Contact Me</h1>
+          <h3>Scan the image below with a QR reader to save my contact information</h3>
+          <img id="qr-vcard" src="images/joshhuffman.vcard.qr.svg">
+          <h3>- or -</h3>
+          <div>
+              <h3><a class="link-inline" href="files/joshua_huffman.vcf" download>Click</a> to download vCard contact information</h3>
+          </div>
+      </div>
+  </body>
+</html>
