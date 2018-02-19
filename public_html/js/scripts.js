10552055159821169287ab4492bf61e9b5b6a23a
Joshua Huffman <j.m.huffman@gmail.com>
2018-02-18 22:04:33 -0800

    scrap sidebar, style header and section 1

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -1,13 +1,17 @@
-$(function(){
-  var activeProject = $('.project-bar li.active');
-  var targetProject = '#' + activeProject.data('target');
-  $(targetProject).show();
-});
+(function($){
+  $(function () {
+    var activeProject = $('.project-bar li.active');
+    var targetProject = '#' + activeProject.data('target');
+    $(targetProject).show();
+    
+    $('.reveal-at-load').removeClass('reveal-at-load');
+  });
 
-$('.project-bar li').click(function(){
-  $('.project-list .project-content').hide();
-  $('.project-bar li').removeClass('active');
-  $(this).addClass('active');
-  var target = '#' + $(this).data('target');
-  $(target).show();
-});
+  $('.project-bar li').click(function () {
+    $('.project-list .project-content').hide();
+    $('.project-bar li').removeClass('active');
+    $(this).addClass('active');
+    var target = '#' + $(this).data('target');
+    $(target).show();
+  });
+})(jQuery);
